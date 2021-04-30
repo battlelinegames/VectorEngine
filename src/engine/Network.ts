2df8a12a9c6e1444e7999ad6bc51a7e3c2ffd708
Rick Battagline <rbattagline@gmail.com>
2021-04-30 10:08:18 -0600

    Added class example and period character

--- src/engine/Network.ts
+++ src/engine/Network.ts
@@ -1,1 +1,5 @@
 // @JairusSW do you want to help me out here?
+
+// The JavaScript needs to be told where the buffer is
+@external("env", "setNetworkPtr")
+declare function setNetworkPtr(ptr: usize);
