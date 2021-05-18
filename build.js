75bfb3e16f0258b7b6a66130ffad9905a00dc58e
Rick Battagline <rbattagline@gmail.com>
2021-05-18 13:24:11 -0600

    Added vec init and vec dev command line

--- build.js
+++ build.js
@@ -1,20 +1,12 @@
-//const { exec } = require("child_process");
 const fs = require('fs');
 const path = require('path');
 const min = require('node-minify');
-//const imagemin = require('imagemin');
-//const imageminPngQuant = require('imagemin-pngquant');
 
 
 
 fs.readdir('./src/engine', (err, files) => {
 
   files.forEach(file => {
-    /*
-    if (file === 'VectorEngine.ts') {
-      fs.copyFileSync('./src/engine/' + file, './index.ts');
-    }
-    else */
     if (path.extname(file) === '.ts') {
       fs.copyFileSync('./src/engine/' + file, './' + path.basename(file));
     }
@@ -23,11 +15,3 @@ fs.readdir('./src/engine', (err, files) => {
 
 fs.copyFileSync('./src/engine/VectorEngine.js', './lib/VectorEngine.js');
 fs.copyFileSync('./src/engine/VectorEngine.js', './lib/VectorEngine.min.js');
-
-/*
-fs.readdir('./src/examples/helloworld', (err, files) => {
-  files.forEach(file => {
-    fs.copyFileSync('./src/examples/helloworld/' + file, './bin/' + path.basename(file));
-  });
-});
-*/
